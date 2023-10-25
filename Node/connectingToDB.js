0e228170b2b2a2a97718b5b41158068633fefea6
L00160435 <L00160435@atu.ie>
2023-10-25 13:25:20 +0100

    Node file to connect to mongo

---
+++ Node/connectingToDB.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware to parse requests before they go to the handlers
+app.use(bodyParser.json());
+
+// MongoDB connection
+mongoose.connect('MongoDB URL Goes Here', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connection Successful');
+});
+
+// Simple Route test
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
