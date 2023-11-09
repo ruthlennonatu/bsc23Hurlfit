fa8a7beeb2a10be2aea2da197e39a59b9a59cfc5
L00156477 <l00156477@atu.ie>
2023-11-09 21:40:59 +0000

    backlog issue #100
    added routes to connect front end and back end allowing data to be transferred, going to look into ways to encrypt the data while it transfers

--- Node/Model/Model.js
+++ Node/Model/Model.js
@@ -1,1 +1,47 @@
-//All database connections and quries will go here
+// Import the MongoDB client
+const { MongoClient } = require('mongodb');
+
+// Connection URI for local MongoDB instance
+const uri = 'mongodb://localhost:27017'; 
+
+// Database Name
+const databaseName = 'hurling'; 
+// Create a new MongoClient
+const client = new MongoClient(uri);
+
+// Connect to MongoDB
+async function connectToMongoDB() {
+  try {
+    await client.connect();
+    console.log('Connected successfully to MongoDB');    
+  } catch (err) {
+    console.error('Error occurred while connecting to MongoDB:', err);
+  }
+}
+
+async function closeConnection(){
+    await client.close();
+}
+
+//these are the only two needed so far 
+exports.SignUp = function(res,req,data){
+    connectToMongoDB().then(
+        function(){
+            //do query here
+        }
+    );
+
+    closeConnection();
+}
+
+
+exports.Login = function(res,req,data){
+    connectToMongoDB().then(
+        function(){
+            //do query here
+            //needs to return some kind ok okay respose to let user log in 
+        }
+    );
+
+    closeConnection();
+}
