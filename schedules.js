7c289502437a9029208da7b8b8d38edd45908f13
Oskar Iszkulo - STUDENT <l00166873@atu.ie>
2023-11-13 17:22:31 +0000

    Worked on the schedule related stuff, issue #14 - unfinished

--- schedules.js
+++ schedules.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json())
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
@@ -16,54 +18,120 @@ db.once('open', () => {
     console.log('Connection Successful');
 });
 
+//function, for rendering the page
 
-db.post('/create-schedule', (req, res) => {
-    //create new schedule
-    //title
-    //trainer
-    //day1
-    //day2
-    //...
-    //day5(or till day7, depends on if weekends are included)
+//res.render('name of html or handlebar page', {'data name seen once recieved': the data you want to send, 'data name seen once recieved': the data you want to send})
 
+//findOne
+//insertOne
+//deleteOne
+
+//need to change the "time" value in sessions to "duration"
+//or something which will allow me to make sure the sessions dont overlap
+
+app.get('/schedules', () => {
+    //get data from database
+    let schedule_list = [];
+    let session_list = [];
+
+    sc = db.collection('schedules')
+    .find()
+    .sort({schedule_title:1})
+    .forEach(schedule => schedule_list.push(schedule))
+    .then(() => {
+        res.status(200).json(schedule_list)
+    })
+    .catch(() => {
+        res.status(500).json({error: 'Could not fetch schedules'})
+    });
+
+    se = db.collection('sessions')
+    .find()
+    .sort({session:1})
+    .forEach(session_obj => session_list.push(session_obj))
+    .then(() => {
+        res.status(200).json(session_list)
+    })
+    .catch(() => {
+        res.status(500).json({error: 'Could not fetch sessions'})
+    });
+
+    //res.redirect('schedules');
+})
+
+//creates and adds a new schedule json object into the database
+app.post('/schedules/create', (req, res) => {
     //idea for the schedules,
     //eg. "tuesday":[['session_name','start_time','duration'], ['session_name','start_time','duration'], ['session_name','start_time','duration']]
 
     //obviously, the sessions shouldn't be able to overlap, will have to deal with that
 
-    //trainee(user) schedule
+    //can use same schedule dictionary for either trainee(user), or trainer
     const schedule = {
-        "program_title":"temp_program_title",//placeholder for the title --req.body.program_title-- or program id related?
-        "trainer_name":"temp_trainer_name",//placeholder for the name --req.body.trainer_name-- or trainer id related?
+        "schedule_title":req.body.schedule_title,//placeholder for the title --req.body.program_title-- or program id related?
+        "schedule_type":req.body.schedule_type,//whether its a schedule for trainees or for the trainers
         "monday":[],
         "tuesday":[],
         "wednesday":[],
         "thursday":[],
         "friday":[]
-    }
+    };
 
     db.collection('schedules')
     .insertOne(schedule)
     .then(result => res.status(201).json(result))
-    .catch(err => {res.status(500).json([err])})
+    .catch(err => {res.status(500).json([err])});
+
+    //res.redirect('schedules');
 });
 
+//delets a schedule json object from the database 
+app.delete('/schedules/delete/:id', (req, res) =>{
+    if(ObjectId.isValid(req.params.id)){
+        db.collection('schedules')
+        .deleteOne({_id: ObjectID(req.params.id)})
+        .then(result => res.status(200).json(result))
+        .catch(err => {res.status(500).json([err])});
+    }
+    else{
+        res.status(500).json({error: 'ID not valid'});
+    }
 
+    //res.redirect('schedules');
+});
 
-db.post('/schedule-add-session', (req, res) => {
+//adds a session to a specific schedule
+app.post('/schedule/add-session/:id', (req, res) => {
     //add a training session to the ___ schedule
+
+    //get the schedule
+    
+
+    //check if the time overlaps
+
+    //add
+
+    //return schedule to database
+
+
+    //res.redirect('schedules');
 });
 
-db.post('/schedule-edit-session', (req, res) => {
+//removes a session from a specific schedule
+app.delete('/schedule/delete-session/:id', (req, res) => {
+    //delete a training session from the ___ schedule
+
+
+    //res.redirect('schedules');
+});
+
+
+/*
+app.post('/schedule-edit-session', (req, res) => {
     //edit a training session in the ___ schedule
 });
 
-db.post('/schedule-delete-session', (req, res) => {
-    //delete a training session from the ___ schedule
-})
-
-/*
-db.post('', (req, res) => {
+app.post('', (req, res) => {
 
 });
 */
