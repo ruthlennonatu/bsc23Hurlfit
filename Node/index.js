8ab7ac6f4970ef1a181fb814939c114d34634ed3
L00166967 <L00166967@atu.ie>
2024-01-29 02:23:19 +0000

    Added comments to the app callbacks in index.js to conform to code standard.

--- Node/index.js
+++ Node/index.js
@@ -17,33 +17,40 @@ app.get("/favicon.ico", function(req, res){
     res.status(204);
 });
 
+//View for signup page - returns /Public/signup.html. Will become a signup form later.
 app.get("/Signup",function(req,res){
     res.sendFile(path.join(__dirname + "/Public/signup.html"));
 });
 
+//View for homepage - returns /Public/index.html. Will become a homepage later.
 app.get("/Index",function(req,res){
     res.sendFile(path.join(__dirname + "/Public/index.html"));
 });
 
+//View for booking a time - currently returns /Public/booking.html. Will become a homepage later.
 app.get("/Booking",function(req,res){
     res.sendFile(path.join(__dirname + "/Public/booking.html"));
 });
 
+//Handles signing up after submitting the signup form.
 app.post("/SignupSubmit",function(req,res){
     var data = req.body
     model.SignUp(req,res,data)
 });
 
+//Handles logging in after submitting the login form.
 app.post("/LoginSubmit",function(req,res){
     var data = req.body
     console.log(data);
     model.Login(req,res,data)
 });
 
+//Function that runs when the server starts.
 app.listen(3000, function () {
     console.log("Server listening on port 3000");
 });
 
+//Handle checking the user's credientials and booking a room - proof of concept, will split into different functions later.
 app.post('/checkUserCredentials', function (req, res){
     var data = req.body
     model.book(req,res,data)
